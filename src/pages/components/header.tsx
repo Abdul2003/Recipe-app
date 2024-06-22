5a5c6170e4717fe3defc31ed3593c714c327c724
Abdul2003 <abdulabbas875@gmail.com>
2024-06-22 15:50:31 +0100

    added sign in and password login

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { UserOutlined, FilterOutlined } from '@ant-design/icons'
+import {
+  UserOutlined,
+  FilterOutlined,
+  GoogleOutlined,
+  EyeOutlined,
+  EyeInvisibleOutlined,
+} from '@ant-design/icons'
 import {
   MenuProps,
   Row,
@@ -10,12 +16,20 @@ import {
   RadioChangeEvent,
   Layout,
   Menu,
+  Input,
+  Modal,
+  message,
 } from 'antd'
 import { Link } from 'react-router-dom'
 import Search from './search'
 import firebase from 'firebase/compat/app'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
 import '../../styles/header.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
@@ -27,6 +41,8 @@ import Fav from './addRecipe'
 function HomeLayout(props) {
   const { Header } = Layout
   const [value, setValue] = useState(1)
+  const [messageApi, contextHolder] = message.useMessage()
+
   function cuisineTypeFilter(cuisineType: RadioChangeEvent) {
     console.log('checked = ', cuisineType.target.value)
     props.filterCuisineType(cuisineType.target.value)
@@ -45,12 +61,100 @@ function HomeLayout(props) {
 
   firebase.initializeApp(initializeFirebase)
   const auth = firebase.auth()
+  const db = firebase.firestore()
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider()
     provider.setCustomParameters({
       prompt: 'select_account',
     })
     auth.signInWithPopup(provider)
+  }
+
+  // Modal Box And SignUp Fields
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [signUpEmail, setSignUpEmail] = useState('')
+  const [signUpPassword, setSignUpPassword] = useState('')
+  const [EmailPlaceholder, setEmailPlaceholder] = useState('Email')
+  const [PasswordPlaceholder, setPasswordPlaceholder] = useState('Password')
+  const [emailStatus, setEmailStatus] = useState(null)
+  const [passwordStatus, setPasswordStatus] = useState(null)
+  const [signUpPasswordType, setSignUpPasswordType] = useState('password')
+  const [signUpEye, setSignUpEye] = useState(<EyeOutlined />)
+
+  const toggleSignupPassword = () => {
+    if (signUpPasswordType == 'password') {
+      setSignUpPasswordType('text')
+      setSignUpEye(<EyeInvisibleOutlined />)
+    } else {
+      setSignUpPasswordType('password')
+      setSignUpEye(<EyeOutlined />)
+    }
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    if (signUpPassword.length < 6) {
+      messageApi.info('Password Should Be At Least Six Characters')
+      if (signUpEmail == '') {
+        setEmailPlaceholder('This Field Should Not Be Empty')
+        setEmailStatus('error')
+      }
+      if (signUpPassword == '') {
+        setPasswordPlaceholder('This Field Should Not Be Empty')
+        setPasswordStatus('error')
+      }
+    } else {
+      createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
+      messageApi.info('Account Created')
+      handleCancel()
+    }
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  // Handle SignIn
+  const [signInEmail, setSignInEmail] = useState('')
+  const [signInPassword, setSignInPassword] = useState('')
+  const [signInEmailPlaceholder, setSignInEmailPlaceholder] = useState('Email')
+  const [signInPasswordPlaceholder, setSignInPasswordPlaceholder] =
+    useState('Password')
+  const [signInEmailStatus, setSignInEmailStatus] = useState(null)
+  const [signInPasswordStatus, setSignInPasswordStatus] = useState(null)
+  const [signInPasswordType, setSignInPasswordType] = useState('password')
+  const [signInEye, setSignInEye] = useState(<EyeOutlined />)
+
+  const signIn = () => {
+    if (signInEmail == '') {
+      setSignInEmailPlaceholder('This Field Should Not Be Empty')
+      setSignInEmailStatus('error')
+    }
+    if (signInPassword == '') {
+      setSignInPasswordPlaceholder('This Field Should Not Be Empty')
+      setSignInPasswordStatus('error')
+    } else {
+      signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+        .then(() => {
+          messageApi.info('Sign In Successful')
+        })
+        .catch((error) => {
+          messageApi.info(error.message)
+        })
+    }
+  }
+
+  const toggleSigninPassword = () => {
+    if (signInPasswordType == 'password') {
+      setSignInPasswordType('text')
+      setSignInEye(<EyeInvisibleOutlined />)
+    } else {
+      setSignInPasswordType('password')
+      setSignInEye(<EyeOutlined />)
+    }
   }
 
   const [user] = useAuthState(auth as any)
@@ -160,18 +264,33 @@ function HomeLayout(props) {
       ],
     },
   ]
-
+  console.log(auth.currentUser)
   const items: MenuProps['items'] = user
     ? [
         {
           key: '1',
-          label: <h1>Hi {auth.currentUser.displayName}</h1>,
+          label: (
+            <>
+              {auth.currentUser.displayName == null ? (
+                <h1 className="text-base font-normal">
+                  Signed In As{' '}
+                  <p className="inline text-gray-500">
+                    {auth.currentUser.email}
+                  </p>
+                </h1>
+              ) : (
+                <h1 className="text-base font-normal">
+                  Hi {auth.currentUser.displayName}
+                </h1>
+              )}
+            </>
+          ),
         },
         {
           key: '2',
           label: (
             <Link to={'/pages/favourites'}>
-              <h1>Favourites</h1>
+              <h1 className="text-base font-normal">Favourites</h1>
             </Link>
           ),
         },
@@ -179,14 +298,18 @@ function HomeLayout(props) {
         {
           key: '3',
           label: (
-            <button
-              onClick={() => {
-                auth.signOut()
-                window.location.reload()
-              }}
-            >
-              Sign out
-            </button>
+            <div className="flex justify-center">
+              <Button
+                type="primary"
+                danger
+                onClick={() => {
+                  auth.signOut()
+                  window.location.reload()
+                }}
+              >
+                Sign out
+              </Button>
+            </div>
           ),
         },
       ]
@@ -194,13 +317,74 @@ function HomeLayout(props) {
         {
           key: '1',
           label: (
-            <button onClick={signInWithGoogle}>Sign In With google</button>
+            <div
+              className="flex justify-center"
+              onClick={(e) => e?.stopPropagation()}
+            >
+              <Input
+                value={signInEmail}
+                onChange={(e) => setSignInEmail(e.target.value)}
+                placeholder={signInEmailPlaceholder}
+                status={signInEmailStatus}
+              />
+            </div>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <div className="" onClick={(e) => e?.stopPropagation()}>
+              <Input
+                className="w-10/12"
+                type={signInPasswordType}
+                value={signInPassword}
+                onChange={(e) => setSignInPassword(e.target.value)}
+                placeholder={signInPasswordPlaceholder}
+                status={signInPasswordStatus}
+              />
+              <p className="inline" onClick={toggleSigninPassword}>
+                {' '}
+                {signInEye}
+              </p>
+            </div>
+          ),
+        },
+        {
+          key: '3',
+          label: (
+            <div
+              className="flex justify-center"
+              onClick={(e) => e?.stopPropagation()}
+            >
+              <Button type="primary" onClick={signIn}>
+                Sign In
+              </Button>
+            </div>
+          ),
+        },
+        {
+          key: '4',
+          label: (
+            <div className="flex justify-center">
+              <Button onClick={signInWithGoogle}>
+                <GoogleOutlined /> Sign In With Google
+              </Button>
+            </div>
+          ),
+        },
+        {
+          key: '5',
+          label: (
+            <div className="flex justify-center">
+              <a onClick={showModal}>Sign Up</a>
+            </div>
           ),
         },
       ]
 
   return (
     <div className="topNav">
+      {contextHolder}
       <Row gutter={{ xs: 0 }}>
         <Col xs={10} sm={8}>
           <Link
@@ -243,6 +427,34 @@ function HomeLayout(props) {
           </Menu>
         </Col>
       </Row>
+      <Modal
+        title="Sign Up"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <label />
+        Email
+        <Input
+          className="m-2"
+          placeholder={EmailPlaceholder}
+          value={signUpEmail}
+          onChange={(e) => setSignUpEmail(e.target.value)}
+          status={emailStatus}
+        />
+        <label>Password</label>
+        <Input
+          className="m-2 w-11/12"
+          type={signUpPasswordType}
+          placeholder={PasswordPlaceholder}
+          value={signUpPassword}
+          onChange={(e) => setSignUpPassword(e.target.value)}
+          status={passwordStatus}
+        />
+        <p className="inline" onClick={toggleSignupPassword}>
+          {signUpEye}
+        </p>
+      </Modal>
     </div>
   )
 }
